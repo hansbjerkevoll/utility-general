07cbb54d2d616950960358df007aa0f9e4227501
Hans Bjerkevoll <hans.bjerkevoll@evry.com>
2020-06-28 20:36:31 +0200

    swiftui color hexstring extension

---
+++ swift/Extensions/Color+HexString.swift
@@ -0,0 +1,26 @@
+//
+//  Color+HexString.swift
+//  CubeTimer
+//
+//  Created by Hans Bjerkevoll on 28/06/2020.
+//  Copyright © 2020 hansbjerkevoll. All rights reserved.
+//
+
+import SwiftUI
+
+extension Color {
+	init(hex: Int, alpha: Double = 1) {
+		let components = (
+			R: Double((hex >> 16) & 0xff) / 255,
+			G: Double((hex >> 08) & 0xff) / 255,
+			B: Double((hex >> 00) & 0xff) / 255
+		)
+		self.init(
+			.sRGB,
+			red: components.R,
+			green: components.G,
+			blue: components.B,
+			opacity: alpha
+		)
+	}
+}
